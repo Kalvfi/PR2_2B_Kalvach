ffde58d42d781d8b96808f484222c3fe803c750f
Filip Kalvach <filip.kalvach@gmail.com>
2024-09-30 12:35:14 +0200

    test

---
+++ 00-Rev-01-pod-limitem/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _00_Rev_01_pod_limitem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cisla = { 1.3, 2.2, -1.5, 1.4, -2.7 };
+            Console.WriteLine(PrumerPodLimitem(cisla, 1.1));
+            Console.WriteLine(PrumerPodLimitem(cisla, -2));
+        }
+
+        public static double PrumerPodLimitem(double[] cisla, double limit)
+        {
+            double soucet = 0;
+            double x = 0;
+
+            for (int i = 0; i < cisla.Length; i++)
+            {
+                if (cisla[i] < limit)
+                {
+                    soucet += cisla[i];
+                    x++;
+                }
+            }
+
+            return soucet / x;
+        }
+    }
+}
---
+++ 00-Rev-01-pred-maximem/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _00_Rev_01_pred_maximem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dobrý den, zadávejte čísla. Pokud chcete přestat zadejte solvo dost");
+
+            int max = -9999999;
+            int predmax = 0;
+            int x = 0;
+            int i = 0;
+
+            while (true) 
+            {
+                string vstup = Console.ReadLine();
+                if (vstup == "dost")
+                {
+                    break;
+                }
+                else if (!int.TryParse(vstup, out int a))
+                {
+
+                }
+                else 
+                {
+                    int cislo = int.Parse(vstup);
+                    if (i == 1) 
+                    {
+                        predmax = cislo;
+                    }
+
+                    if (cislo > max)
+                    {
+                        max = cislo;
+                        predmax = x;
+                        x = cislo;
+                        i++;
+                    }
+                    else 
+                    {
+                        x = cislo;
+                    }
+                    
+                }
+            }
+
+            if (max == -9999999)
+            {
+                Console.WriteLine("Nebylo vloženo žádné číslo.");
+            }
+            else 
+            {
+                Console.WriteLine(predmax);
+            }
+        }
+    }
+}
